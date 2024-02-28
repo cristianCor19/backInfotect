d1ef830527e0a447919599701fd39192126a7d0e
cristianCor19 <cordobac96@gmail.com>
2024-02-27 20:10:22 -0500

    cors changes

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(cookieParser())
 
 const corsOptions = {
     credentials: true,
-    origin: ['http://localhost:5173', 'http://localhost:5174']
+    origin: ['https://frontend-client-y89z.vercel.app', 'http://localhost:5174']
 };
 
 app.use(cors(corsOptions));
