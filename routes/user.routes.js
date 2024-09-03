e3fa9449bc9a448f6abfdc2224c67cad935e4e51
cristianCor19 <cordobac96@gmail.com>
2024-09-02 22:30:44 -0500

    Remove routes of methods to session

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,16 +5,11 @@ import {
     saveUser,
     findUserById,
     deleteUser,
-    loginUser,
-    sendMailRecoveryPass,
     profile,
-    verifyToken,
-    logout,
-    verifyUser,
-    updatePasswordRecovery
+    
 } from '../controllers/controll_user.js'
 //modelo para la validacion del cuerpo de las peticiones
-import { registerUserShema, loginUserSchema } from '../shemas/user.schema.js'
+import { registerUserShema } from '../shemas/user.schema.js'
 //funcion para validar el modelo de las peticiones
 import { validateSchema } from '../middlewares/validatorRequest.js'
 import { authRequired } from '../middlewares/validateToken.js'
@@ -22,16 +17,12 @@ import { authRequired } from '../middlewares/validateToken.js'
 const router = Router()
 //configuracion de rutas para la parte de usuarios
 router.post('/registerUser', validateSchema(registerUserShema), saveUser)
-router.post('/login', validateSchema(loginUserSchema), loginUser)
-router.get('/verifyUser/:token', verifyUser)
-router.get('/verify/:token', verifyToken)
-router.post('/logout', verifyToken, logout)
+
 router.get('/', obtainAllUsers)
 router.get('/profile/:token', authRequired, profile)
 
 router.delete('/:id', deleteUser)
-router.put('/updatePasswordRecovery', updatePasswordRecovery)
-router.post('/sendEmail/email', sendMailRecoveryPass)
+
 router.get('/:id', findUserById)
 
 
