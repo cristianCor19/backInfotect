4c969731bd8e9311cd58e97f5a606714044dfaea
cristianCor19 <cordobac96@gmail.com>
2024-09-02 22:30:09 -0500

    Create routes to method of session

---
+++ routes/session.routes.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { loginUserSchema } from '../shemas/user.schema.js'
+
+import {
+    loginUser,
+    sendMailRecoveryPass,
+    verifyToken,
+    logout,
+    verifyUser,
+    updatePasswordRecovery 
+    
+} from "../controllers/controll_session";
+
+const router = Router();
+
+router.post('/login', validateSchema(loginUserSchema), loginUser)
+router.get('/verifyUser/:token', verifyUser)
+router.get('/verify/:token', verifyToken)
+router.post('/logout', verifyToken, logout)
+router.put('/updatePasswordRecovery', updatePasswordRecovery)
+router.post('/sendEmail/email', sendMailRecoveryPass)
