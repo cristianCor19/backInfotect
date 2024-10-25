5e805106f713eef6bd5b133aad73d6ebf5fe9e1e
cristianCor19 <cordobac96@gmail.com>
2024-10-25 10:30:25 -0500

    test: test conexion api

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cookieParser())
 
 const corsOptions = {
     credentials: true,
-    origin: [process.env.CLIENT, process.env.ADMIN, "http://localhost:5173"]
+    origin: ["https://frontend-client-wine.vercel.app", process.env.ADMIN, "http://localhost:5173"]
 };
 
 app.use(cors(corsOptions));
